67d17ea9f27dde5fbf76f656ed528900c556b603
Drawain <andras.fincza@emarsys.com>
2016-09-12 00:40:21 +0200

    feat(initial)

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const co = require('co');
+const cli = require('./lib/cli').create();
+const options = cli.getArguments();
+const chalk = require('chalk');
+const log = console.log;
+const logEpic = (epic) => log(chalk.bold('> ' + epic));
+
+const Deis = require('./lib/deis');
+const Heroku = require('./lib/heroku');
+const deleteFolder = require('rimraf').sync;
+
+
+co(function*() {
+  const deis = Deis.create();
+
+  logEpic('Connecting to Heroku');
+  const heroku = yield Heroku.connectTo(options.herokuApiToken);
+
+  logEpic('Cloning application from Heroku');
+  const clonePath = yield heroku.cloneAppToTemp(options.herokuApp);
+
+  logEpic('Get app configuration from Heroku');
+  const herokuAppConfig = yield heroku.getAppConfig(options.herokuApp);
+
+  logEpic('Create Deis application');
+  yield deis.createApplication(options.deisApp);
+
+  logEpic('Set app configuration');
+  yield deis.setConfig(options.deisApp, herokuAppConfig);
+
+  logEpic('Pushing application to Deis');
+  yield deis.pushApplication(options.deisApp, clonePath);
+
+  logEpic('Migration done successfully!');
+  logEpic('\nSome useful commands:');
+  log(`Open: ${chalk.dim(`deis open -a ${options.deisApp}`)}`);
+  log(`Scale up: ${chalk.dim(`deis scale cmd=2 -a ${options.deisApp}`)}`);
+  log(`Destroy: ${chalk.dim(`deis destroy --app=${options.deisApp} --confirm=${options.deisApp}`)}`);
+}).catch(function(err) {
+  log(`\n\n${chalk.red(`There was an error while migrating :(`)}\n\n`);
+  log(err);
+});
+
+
+function onExit() {
+    deleteFolder('tmp');
+    process.exit();
+}
+
+process.on('exit', onExit);
+process.on('SIGINT', onExit);
+process.on('uncaughtException', onExit);
+
+
